0f2eb818aac637c49235613f5700412c95917d6c
Justin Bergen <itsniper@gmail.com>
2024-11-18 21:16:25 -0700

    Added test function to main class

---
+++ Sources/ReliaBLE.swift
@@ -0,0 +1,18 @@
+//
+//  ReliaBLE.swift
+//  ReliaBLE
+//
+//  Created by Justin Bergen on 11/18/24.
+//
+
+import Foundation
+
+public class ReliaBLE {
+    public init() {
+        
+    }
+    
+    public func testFunction() -> String {
+        return "Hello, this is ReliaBLE!"
+    }
+}
--- Tests/ReliaBLETests/ReliaBLETests.swift
+++ Tests/ReliaBLETests/ReliaBLETests.swift
@@ -1,6 +1,8 @@
 import Testing
 @testable import ReliaBLE
 
-@Test func example() async throws {
+@Test func correctFunction() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let package = ReliaBLE()
+    #expect(package.testFunction() == "Hello, this is ReliaBLE!", "Incorrect response string")
 }
