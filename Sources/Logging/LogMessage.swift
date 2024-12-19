b135235a7f64640cf6d1a11d690d4e689b7deb2f
Justin Bergen <justin@five3apps.com>
2024-12-19 10:31:18 -0700

    Added custom message type with typed tags

--- Sources/Logging/LogMessage.swift
+++ Sources/Logging/LogMessage.swift
@@ -1,8 +1,8 @@
 //
-//  ReliaBLEManager.swift
+//  LogMessage.swift
 //  ReliaBLE
 //
-//  Created by Justin Bergen on 11/18/24.
+//  Created by Justin Bergen on 12/15/24.
 //
 //  Copyright (c) 2024 Five3 Apps, LLC <justin@five3apps.com>
 //
@@ -12,10 +12,10 @@
 //  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 //  copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
-//
+//  
 //  The above copyright notice and this permission notice shall be included in all
 //  copies or substantial portions of the Software.
-//
+//  
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,28 +24,58 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-import Foundation
-
 @preconcurrency import Willow
 
-/// The main entry point for the ReliaBLE library.
-public class ReliaBLEManager {
-    public let loggingService: LoggingService
-    
-    private let log: Logger
-    
-    public init(config: ReliaBLEConfig = ReliaBLEConfig()) {
-        loggingService = LoggingService(levels: config.logLevels, writers: config.logWriters, queue: config.logQueue)
-        loggingService.enableLogging(config.loggingEnabled)
-        log = loggingService.willowLogger
+enum LogTag {
+    enum Category: String {
+        case scanning
+        case connection
     }
     
-    /// This is a test function that returns a string.
-    /// - Returns: A string that says "Hello, this is ReliaBLE!"
-    public func testFunction() -> String {
-        log.debugMessage("testFunction() called")
-        log.debug(LogMessage(tags: [.category(.connection), .category(.scanning), .peripheral("123")], message: "testFunction() called"))
-        
-        return "Hello, this is ReliaBLE!"
+    case category(Category)
+    case peripheral(String)
+
+    var attribute: (key: String, value: String) {
+        switch self {
+        case .peripheral(let peripheralId):
+            return ("Peripheral", peripheralId)
+        case .category(let category):
+            return ("Category", category.rawValue)
+        }
+    }
+}
+
+struct LogMessage: Willow.LogMessage {
+    var tags: [LogTag]?
+    var message: String
+    
+    // MARK: - LogMessage
+    
+    var name: String {
+        message
+    }
+    
+    var attributes: [String : Any] {
+        guard let tags else {
+            return [:]
+        }
+
+        var result: [String: Any] = [:]
+        var categories: [String] = []
+
+        for tag in tags {
+            let (key, value) = tag.attribute
+            if key == "Category" {
+                categories.append(value)
+            } else {
+                result[key] = value
+            }
+        }
+
+        if !categories.isEmpty {
+            result["Category"] = categories.joined(separator: ",")
+        }
+
+        return result
     }
 }
